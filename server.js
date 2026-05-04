bab7152582ad6704b104acffd2679fe522dc9b96
Phat <cuongphat234@gmail.com>
2026-05-04 16:13:58 +0700

    add CORS

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const mysql = require("mysql2");
 const app = express();
 app.use(express.json());
 
+// ================= CORS =================
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 // ================= DATABASE =================
 
 const db = mysql.createConnection({
